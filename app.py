f7a2a5cf5f7744edb5797beb307aa7c6bffe0da3
Catiussia Nascimento <catiussia.nascimento@hsl.org.br>
2025-04-22 18:31:34 -0300

    add files

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import sqlite3
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 def init_db():
@@ -21,7 +23,7 @@ init_db()
 
 @app.route('/')
 def home_page():
-    return '<h2>Bem-vindo à API de Livros!</h2>'
+    return render_template('index.html')
 
 
 @app.route('/doar', methods=['POST'])
@@ -64,6 +66,19 @@ def listar_livros():
         }
         livros_formatados.append(dicionario_livros)
     return jsonify(livros_formatados)
+
+
+@app.route('/livros/<int:livro_id>', methods=['DELETE'])
+def deletar_livro(livro_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM livros WHERE id = ?", (livro_id,))
+        conn.commit()
+
+    if cursor.rowcount == 0:
+        return jsonify({"erro": "Livro não encontrado"}), 404
+
+    return jsonify({"menssagem": "Livro excluido com sucesso"}), 200
 
 
 if __name__ == '__main__':
